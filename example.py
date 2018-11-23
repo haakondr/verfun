95a616f17183dcca33fe08889c49197cff558988
Håkon Drolsum Røkenes <hakon.rokenes@niva.no>
2018-11-23 08:52:58 +0100

    Added simple example and updated readme

--- example.py
+++ example.py
@@ -1,29 +1,5 @@
-# Verfun
-
-Verfun is a util for generating a checksum for a python function ignoring whitespace, comments, variable names, etc.
-
-## Great, why is this useful?
-
-Given a use case where you want to 
-
-1. Version an algorithm that did something to your data
-2. Rerun said algorithm on affected data if the algorithm changes
-3. not rerun if you only add comments, refactor variable names, etc
-
-## Features:
-
-Generating a md5 checksum of a given function, ignoring the following:
-
-- function name
-- docstrings and comments
-- whitespace
-- indentation
-
-## Example usage
-Example taken from [example.py](example.py)
-
-```
 from src.verfun import version_hash_for_function
+
 
 def some_strange_looking_function(param1, callback_fn):
     tail = param1[-1]
@@ -38,7 +14,7 @@ def same_strange_looking_function(param1, callback_fn):
     tail = param1[-1]
     # return the callback value from the tail of param whatever that is
     return callback_fn(tail)
-   
+
 
 # generate checksum for each function
 print(version_hash_for_function(some_strange_looking_function))
@@ -46,4 +22,3 @@ print(version_hash_for_function(some_strange_looking_function))
 
 print(version_hash_for_function(same_strange_looking_function))
 # c4cb4d2638d790016cbee29a6ed5af8c
-```
