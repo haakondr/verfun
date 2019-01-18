6a07095612044456c853fdf70377e3343d3fd73c
Håkon Drolsum Røkenes <hakon.rokenes@niva.no>
2019-01-18 11:30:54 +0100

    Added setup.py for packaging

---
+++ setup.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='verfun',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='0.1',
+
+    description="Verfun is a util for generating a checksum for a python function",
+
+    # The project's main homepage.
+    url='https://github.com/haakondr/verfun',
+
+    author='Håkon Drolsum Røkenes',
+
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Code Tools :: checksums',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: MIT',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    install_requires=[],
+    test_suite='tests',
+)
