2eda59429f1556e2a56e330e961fefe742eb04b9
Håkon Drolsum Røkenes <hakon.rokenes@niva.no>
2020-01-01 22:57:30 +0100

    bumped version to 0.2.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.2.0',
+    version='0.2.1',
 
     description="Verfun is a util for generating a checksum for a python function",
     long_description=long_description,
